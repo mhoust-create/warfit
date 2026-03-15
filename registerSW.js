2f124657288296cdfa19e79637f2be913a4ed6dd
mhoust-create <mhoust@gmail.com>
2026-03-15 11:34:13 +0000

    Fix base path for GitHub Pages

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/warfit/sw.js', { scope: '/warfit/' })})}
